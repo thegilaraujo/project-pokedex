72e96b6304c8c96f30de888efd2364c5db694859
Gildevan Araujo <gildevan.araujo@outlook.com.br>
2022-08-01 22:56:02 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchhPokemon = async pokemon => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIResponse.status == 200) {
+    const data = await APIResponse.json()
+    return data;
+  }
+}
+
+const renderPokemon = async pokemon => {
+  pokemonName.innerHTML = 'Carregando...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchhPokemon(pokemon)
+
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Não encontrado 😞';
+    pokemonNumber.innerHTML = '';
+  }
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase())
+});
+
+btnPrev.addEventListener('click', event => {
+  if (searchPokemon > 1){
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+btnNext.addEventListener('click', event => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
---
+++ readmed.md
@@ -0,0 +1,7 @@
+# Projeto Pokedéx
+
+Este projeto foi com o intuito de aprimorar os conhecimentos referente a **HTML**, **CSS** e **JavaScript**.
+
+Projeto criado atráves do canal [Manual do Dev](https://www.youtube.com/c/ManualdoDev).
+
+E-mail para contato: gildevan.araujo@outlook.com.br
